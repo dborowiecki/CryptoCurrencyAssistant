532bc900d6cf1445cc4908aae37cda4f9c4cc6bd
DMN <damiandamian97@wp.pl>
2018-06-03 01:16:39 +0200

    Added trend lines

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -8,9 +8,11 @@ import play.data.Form;
 import play.data.FormFactory;
 import play.mvc.*;
 import services.DataFetcher;
+import services.DateFormater;
 import views.html.Diagram.*;
 
 import javax.inject.Inject;
+import java.util.Objects;
 
 
 /**
@@ -28,39 +30,48 @@ public class HomeController extends Controller {
     @Inject
     FormFactory formFactory;
     public Result index() {
-        return ok(views.html.index.render());
+        return diagram();
     }
 
     public Result diagram(){
         DynamicForm requestData = formFactory.form().bindFromRequest();
-        String firstname = requestData.get("dateFrom");
-        String lastname = requestData.get("dateTo");
-        System.out.print(firstname+" "+lastname);
-        String dateFrom = firstname;//"2015-11-10";
-        String dateTo = lastname;//"2018-03-15";
-
+        String dateFrom = requestData.get("dateFrom");
+        String dateTo = requestData.get("dateTo");
+        Integer showTrends = requestData.get("showTrends")!= null ? 1 : 0;
         DynamicForm period = formFactory.form();
-        period.bindFromRequest("dateFrom", "dateTo");
-
+        period.bindFromRequest("dateFrom", "dateTo", "showTrends");
+        try {
+            if (Objects.equals(dateTo, "")) {
+                dateTo = DateFormater.dateToString(DateFormater.getCurrentDate(), "yyyy-MM-dd");
+            }
+            if (Objects.equals(dateFrom, "")) {
+                dateFrom = DateFormater.dateToString(
+                        DateFormater.addDays(DateFormater.getCurrentDate(),-9), "yyyy-MM-dd");
+            }
+        } catch (Exception e){
+            System.err.println("Data format is invalid");
+        }
         DiagramModel d = new DiagramModel("Coin Trends");
         CurrencyLine newLine;
 
         newLine = CurrencyLine.createNewLine("BTCUSD", dateFrom, dateTo);
         d.addCurrencyLine(newLine);
-        newLine.setColor(255,0,0);
-
-        //Test trenLine
-        d.addTrendLine(d.currencyLines.get(0), "2018-05-01", "2018-05-02");
+        newLine.setColor(220,0,0);
 
         newLine = CurrencyLine.createNewLine("ETHUSD", dateFrom, dateTo);
         d.addCurrencyLine(newLine);
-        newLine.setColor(0,255,0);
+        newLine.setColor(0,220,0);
 
         newLine = CurrencyLine.createNewLine("LTCUSD", dateFrom, dateTo);
         d.addCurrencyLine(newLine);
-        newLine.setColor(0,0, 255);
+        newLine.setColor(0,0, 220);
+        if(showTrends==1) {
+            d.createWeeklyTrendLines(d.currencyLines.get(0));
+            d.createWeeklyTrendLines(d.currencyLines.get(1));
+            d.createWeeklyTrendLines(d.currencyLines.get(2));
+        }
 
-        return ok(diagram.render(firstname, lastname, d, period));
+        return ok(diagram.render(dateFrom, dateTo, showTrends, d, period));
         //return ok("hello");
     }
 
